465b0ed9a68a5ee049d07092c33fe3be376d3955
kazdenleo <kazakov-denis@list.ru>
2023-02-19 14:33:27 +0300

    Доработки финальные

--- js/index.js
+++ js/index.js
@@ -6,6 +6,50 @@ btn.addEventListener("click", () => {
     popup.classList.add('popup_active');
 })
 
-exit.addEventListener("click", () => {
+function closePopup() {
     popup.classList.remove('popup_active');
+}
+
+exit.addEventListener("click", () => {
+    closePopup();
+})
+
+popup.addEventListener('mousedown', (e) => {
+	var container = document.querySelector('.popup__container');
+    if (!container.contains(e.target) & popup.classList.contains('popup_active')) {
+        closePopup();
+    }
+});
+
+const langBtn = document.querySelector('.header__lang');
+const langList = document.querySelector('.header__language-items');
+langBtn.addEventListener("click", () => {
+    langList.classList.toggle('header__language-items_active');
+})
+
+window.addEventListener('mousedown', (e) => {
+	var container = document.querySelector('.header__language-items');
+    if (!container.contains(e.target) & langList.classList.contains('header__language-items_active')) {
+        langList.classList.remove('header__language-items_active');
+    }
+});
+
+//Форма
+const popupBtns = document.querySelectorAll('.popupbtn');
+const line = document.querySelector('.popup__line');
+const popupInfo = popup.querySelectorAll('.popup__pay-info')
+popupBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (btn.classList.contains('popupbtn_left')) {
+            line.classList.remove('popup__line_right')
+            line.classList.add('popup__line_left')
+            popupInfo[1].style.display = "none";
+            popupInfo[0].style.display = "flex";
+        } else if (btn.classList.contains('popupbtn_right')) {
+            line.classList.remove('popup__line_left')
+            line.classList.add('popup__line_right')
+            popupInfo[0].style.display = "none";
+            popupInfo[1].style.display = "flex";
+        }
+    })
 })
